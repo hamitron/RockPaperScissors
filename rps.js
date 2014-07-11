dc1db22268585994e077e7c49e45a3317b60eef6
hamitron <hqkarl@gmail.com>
2014-07-10 20:33:34 -0700

    computer can choose

--- rps.js
+++ rps.js
@@ -1,10 +1,15 @@
 var choices = ["rock", "paper", "scissors"];
 
+function compuChoice(choice) {
+  var computerChoice= Math.floor(Math.random() * choice.length);
+  	document.getElementsByClassName("shape-choice")[computerChoice].style.backgroundColor = "blue";
+}
 
 
 
 function mouseOverColor() {
 	document.getElementsByClassName("shape-choice")[0].style.backgroundColor = "rgba(191,255,255,0.8)";
+	compuChoice(choices);
 };
 function mouseOverColor1() {
 	document.getElementsByClassName("shape-choice")[1].style.backgroundColor = "rgba(191,255,255,0.8)";
@@ -18,7 +23,3 @@ function mouseOverColor2() {
 // };
 
 
-function compuChoice(choice) {
-  var computerChoice= Math.floor(Math.random() * choice.length);
-  	alert(choices[computerChoice])
-}
