6bc7e6f32b0db3591fe54ddff46facb9e40a7ed7
hamitron <hqkarl@gmail.com>
2014-07-10 20:27:06 -0700

    javascript progress

--- rps.js
+++ rps.js
@@ -1,5 +1,23 @@
 var choices = ["rock", "paper", "scissors"];
 
+
+
+
+function mouseOverColor() {
+	document.getElementsByClassName("shape-choice")[0].style.backgroundColor = "rgba(191,255,255,0.8)";
+};
+function mouseOverColor1() {
+	document.getElementsByClassName("shape-choice")[1].style.backgroundColor = "rgba(191,255,255,0.8)";
+};
+function mouseOverColor2() {
+	document.getElementsByClassName("shape-choice")[2].style.backgroundColor = "rgba(191,255,255,0.8)";
+};
+
+// function mouseOverColor2() {
+// 	document.getElementById(this).style.backgroundColor = "rgba(191,255,255,0.8)";
+// };
+
+
 function compuChoice(choice) {
   var computerChoice= Math.floor(Math.random() * choice.length);
   	alert(choices[computerChoice])
