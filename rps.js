984d6e4441363b3cab4ca16b597960ef0af6b275
hamitron <hqkarl@gmail.com>
2014-07-10 21:05:19 -0700

    I forgot how to play rps, but i made some serious progress

--- rps.js
+++ rps.js
@@ -1,22 +1,26 @@
 var choices = ["rock", "paper", "scissors"];
 
-function compuChoice(choice) {
-  var computerChoice= Math.floor(Math.random() * choice.length);
-  	document.getElementsByClassName("shape-choice")[computerChoice].style.backgroundColor = "blue";
-}
 
 
 
 function mouseOverColor() {
 	document.getElementsByClassName("shape-choice")[0].style.backgroundColor = "rgba(191,255,255,0.8)";
 	compuChoice(choices);
+
 };
 function mouseOverColor1() {
 	document.getElementsByClassName("shape-choice")[1].style.backgroundColor = "rgba(191,255,255,0.8)";
+	compuChoice(choices);
 };
 function mouseOverColor2() {
 	document.getElementsByClassName("shape-choice")[2].style.backgroundColor = "rgba(191,255,255,0.8)";
+	compuChoice(choices);
 };
+
+function compuChoice(choice) {
+  var computerChoice= Math.floor(Math.random() * choice.length);
+  document.getElementsByClassName("shape-choice")[computerChoice].style.backgroundColor = "red";
+}
 
 // function mouseOverColor2() {
 // 	document.getElementById(this).style.backgroundColor = "rgba(191,255,255,0.8)";
