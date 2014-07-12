036cccb98bb0fe868b2013191fdf6cf6a0f0b403
hamitron <hqkarl@gmail.com>
2014-07-11 17:05:36 -0700

    with game checker, allows for multiple games to be plaeyd without reset

--- rps.js
+++ rps.js
@@ -1,18 +1,32 @@
 var choices = [0, 1, 2];
 var outcomeUser 
 var computerChoice
+var gamePlayed = false;
+
+function gameChecker() {
+	if (gamePlayed == true) {
+		playAgain();
+	} else {
+		gamePlayed = false ;}
+}
+
+
 
 function mouseOverColor() {
+	gameChecker();
 	document.getElementsByClassName("shape-choice")[0].style.backgroundColor = "rgba(20,204,204,0.5)";
 	outcomeUser = 0;
 	compuChoice(outcomeUser);
+
 };
 function mouseOverColor1() {
+	gameChecker();
 	document.getElementsByClassName("shape-choice")[1].style.backgroundColor = "rgba(20,204,204,0.5)";
 	outcomeUser = 1;
 	compuChoice(outcomeUser);
 };
 function mouseOverColor2() {
+	gameChecker();
 	document.getElementsByClassName("shape-choice")[2].style.backgroundColor = "rgba(20,2204,204,0.5)";
 	outcomeUser = 2;
 	compuChoice(outcomeUser);
@@ -29,13 +43,16 @@ function compuChoice(choice) {
 		(outcomeUser == 1 && computerChoice == 2 ) ||
 		(outcomeUser == 2 && computerChoice == 0 )) {
 			document.getElementsByClassName("whoWins")[0].innerHTML = "<p>Computer Wins!</p>";
+			gamePlayed = true;
 	} else if (
 		(outcomeUser == 0 && computerChoice == 2 ) ||
 		(outcomeUser == 1 && computerChoice == 0 ) ||
 		(outcomeUser == 2 && computerChoice == 1 )) {
 			document.getElementsByClassName("whoWins")[0].innerHTML = "<p>You Win!</p>";
+			gamePlayed = true;
 	} else {
 		document.getElementsByClassName("whoWins")[0].innerHTML = "<p>How did we get here?</p>";
+
 	}
 
 }
@@ -43,8 +60,7 @@ function compuChoice(choice) {
 
 function playAgain () {
 	document.getElementsByClassName("whoWins")[0].innerHTML= "<p>Who wins?</p>";
+	gamePlayed = false;
 	for (i = 0; i < 3; i++) { document.getElementsByClassName("shape-choice")[i].style.backgroundColor = "white";};
-	
-
-
+		
 }
